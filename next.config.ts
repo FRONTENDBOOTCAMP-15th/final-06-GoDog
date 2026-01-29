878e19a85b5b8f2b5b76d523d4602ea926edbfb3
koeunbyeol <silverstar8987@gmail.com>
2026-01-29 14:20:17 +0900

    이미지 호스트 추가

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "fesp-api.koyeb.app",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+    ],
+  },
   async rewrites() {
     return [
       {
